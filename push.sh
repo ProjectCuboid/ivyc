ca9813a922c8301fd333fbb64e8eba921067017a
ProjectCuboid <itzcubical.code@gmail.com>
2025-11-25 19:45:37 +0400

    SH File PUSH

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 
 git init
 git add .
-git commit -m "Initial commit"
+git commit -m "SH File PUSH"
 git branch -M main
-git remote add origin https://github.com/ProjectCuboid/ivyc.git
+git remote add origin https://github.com/ProjectCuboid/ivyc.git --quiet
 git push -u origin main
