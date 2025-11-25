490b1e6fbe0edddcfa0ead78c9009b5948ca7e2e
ProjectCuboid <itzcubical.code@gmail.com>
2025-11-25 19:47:11 +0400

    SH File PUSH

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
-
 git init
 git add .
-git commit -m "SH File PUSH"
+git commit -m "SH File PUSH" --quiet
 git branch -M main
-git remote add origin https://github.com/ProjectCuboid/ivyc.git --quiet
-git push -u origin main
+
+if git remote get-url origin > /dev/null 2>&1; then
+    echo "Remote 'origin' already exists, skipping..."
+else
+    git remote add origin https://github.com/ProjectCuboid/ivyc.git
+fi
+
+git push -u origin main --quiet
